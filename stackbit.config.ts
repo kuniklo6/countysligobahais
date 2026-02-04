dc678758ea5d0c6168df0938ea6d6abbc8282ea5
James Kuniklo <kuniklo.vt@gmail.com>
2026-02-04 13:23:57 +0000

    Added Netlify Visual Editor

---
+++ stackbit.config.ts
@@ -0,0 +1,23 @@
+import { defineStackbitConfig } from "@stackbit/types";
+import { SanityContentSource } from "@stackbit/cms-sanity";
+
+export default defineStackbitConfig({
+    stackbitVersion: "~0.6.0",
+    ssgName: "nextjs",
+    nodeVersion: "20",
+    contentSources: [
+        new SanityContentSource({
+            rootPath: __dirname,
+            studioPath: __dirname + "/studio",
+            projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+            token: process.env.SANITY_ACCESS_TOKEN!,
+            dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+            studioUrl: "https://county-sligo-bahais.netlify.app/studio",
+        }),
+    ],
+    modelExtensions: [
+        { name: "home", type: "page", urlPath: "/" },
+        { name: "about", type: "page", urlPath: "/about" },
+        { name: "post", type: "page", urlPath: "/blog/{slug}" },
+    ],
+});
